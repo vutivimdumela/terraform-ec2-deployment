d725fc075ae9001e8906f36c426a91ac8fabae40
vutivimdumela <millicentmdumela@gmail.com>
2025-11-11 16:03:07 +0200

    vutivi

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,24 @@
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.web_server.id
-}
+# output "instance_id" {
+#   description = "ID of the EC2 instance"
+#   value       = aws_instance.web_server.id
+# }
 
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.web_server.public_ip
-}
+# output "instance_public_ip" {
+#   description = "Public IP address of the EC2 instance"
+#   value       = aws_instance.web_server.public_ip
+# }
 
-output "instance_public_dns" {
-  description = "Public DNS name of the EC2 instance"
-  value       = aws_instance.web_server.public_dns
-}
+# output "instance_public_dns" {
+#   description = "Public DNS name of the EC2 instance"
+#   value       = aws_instance.web_server.public_dns
+# }
 
-output "security_group_id" {
-  description = "ID of the security group"
-  value       = aws_security_group.ec2_sg.id
-}
+# output "security_group_id" {
+#   description = "ID of the security group"
+#   value       = aws_security_group.ec2_sg.id
+# }
 
-output "website_url" {
-  description = "URL to access the web server"
-  value       = "http://${aws_instance.web_server.public_ip}"
-}
+# output "website_url" {
+#   description = "URL to access the web server"
+#   value       = "http://${aws_instance.web_server.public_ip}"
+# }
